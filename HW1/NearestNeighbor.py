49eb708c0b066beb3be6be3cf2906d8eeaaa234b
Roei Herzig <roeih@traxretail.com>
2016-11-25 21:50:27 +0200

    split data to train and test

--- HW1/NearestNeighbor.py
+++ HW1/NearestNeighbor.py
@@ -19,12 +19,30 @@ def get_data_and_labels():
 
 
 def visualize(image):
-    """This function visualize the photo"""
+    """
+    This function visualize the photo
+    :param image: the image
+    """
     img = Image.fromarray(np.reshape(image, (28, 28)))
     img.show()
+
+
+def get_train_and_test_data(data, labels):
+    """
+    :param data: data
+    :param labels: labels
+    :return:
+    """
+    idx = np.random.RandomState(0).choice(70000, 11000)
+    train = data[idx[:10000], :]
+    train_labels = labels[idx[:10000]]
+    test = data[idx[10000:], :]
+    test_labels = labels[idx[10000:]]
+    return train, train_labels, test, test_labels
 
 if __name__ == '__main__':
     print 'start'
     data, labels = get_data_and_labels()
-    visualize(data[2])
+    train, train_labels, test, test_labels = get_train_and_test_data(data, labels)
+
     print 'end'
