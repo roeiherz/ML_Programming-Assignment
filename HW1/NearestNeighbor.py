805948dc8ea8384cc975b991b93fa06ebaa4b207
Roei Herzig <roeih@traxretail.com>
2016-11-25 20:34:39 +0200

    add method to get data and labels from MNIST dataset

--- HW1/NearestNeighbor.py
+++ HW1/NearestNeighbor.py
@@ -1,1 +1,15 @@
+import numpy as np
+from sklearn.datasets import fetch_mldata
+
 __author__ = 'roeiherz'
+
+
+def get_data_and_labels():
+    mnist = fetch_mldata('MNIST original')
+    data = mnist['data']
+    labels = mnist['target']
+    return data, labels
+
+if __name__ == '__main__':
+    data, labels = get_data_and_labels()
+
