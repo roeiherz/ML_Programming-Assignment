f88ad77e2b765e9988dd1ad673ea2aad87c94fe4
Roei Herzig <roeih@traxretail.com>
2016-12-12 22:32:44 +0200

    svm question 2 to the main

--- HW2/q2.py
+++ HW2/q2.py
@@ -7,9 +7,6 @@ import os
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.svm import LinearSVC
-from HW2.Perceptron import Perceptron
-
-NOF_ITERATIONS = 100
 
 
 def get_train_validation_test_data():
@@ -71,26 +68,18 @@ def plot_graph(error_lst, x_lst, file_name='', label='', title='', ylabel='', xl
     plt.savefig('{}.png'.format(file_name))
 
 
-def part_a(org_train, org_train_labels, org_validation, org_validation_labels):
-    """
-    This function running the part A - training SVM on train samples
-    :param org_train: original train sample
-    :param org_train_labels: original train labels
-    :param org_validation: original validation samples
-    :param org_validation_labels: original validation labels
-    :return:
-    """
+if __name__ == '__main__':
+    n_lst = [5, 10, 50, 100, 500, 1000, 5000]
+
+    # Get train, validation and test data
+    org_train, org_train_labels, org_validation, org_validation_labels, org_test, org_test_labels = get_train_validation_test_data()
 
     c_list = np.array(list(range(1, 500, 5))).astype("float32") * 1e-8
-
-    # BEST C
-    # Accuracy is: 0.991167301697 for c:2e-07
 
     training_acc_lst = []
     validating_acc_lst = []
 
     for c in c_list:
-        # c = math.pow(10, c_log)
         svm = LinearSVC(loss='hinge', fit_intercept=False, C=c)
         svm.fit(org_train, org_train_labels)
 
@@ -100,102 +89,11 @@ def part_a(org_train, org_train_labels, org_validation, org_validation_labels):
         validating_acc_lst.append(validating_acc)
 
     plt.figure()
-    # plot_graph(training_acc_lst, c_list, "q2_part_a", "", "Accuracy vs C for SVM", "Accuracy", "C")
-    # plot_graph(validating_acc_lst, c_list, "q2_part_a", "", "Accuracy vs C for SVM", "Accuracy", "C")
+    plot_graph(training_acc_lst, c_list, "q2_part_a", "", "Accuracy vs C for SVM", "Accuracy", "C")
+    plot_graph(validating_acc_lst, c_list, "q2_part_a", "", "Accuracy vs C for SVM", "Accuracy", "C")
     best_acc_indx = validating_acc_lst.index(max(validating_acc_lst))
     best_c = c_list[best_acc_indx]
     print "The best c is {} for error: {}".format(best_c, max(validating_acc_lst))
 
     plt.figure()
-    plt.imshow(np.reshape(svm.coef_, (28, 28)), interpolation='nearest', cmap='gray')
-    print 'debug'
-
-
-def train_and_test(train, train_labels, test, test_labels):
-    """
-    This function will train the Perceptron and
-    :param train: train samples
-    :param train_labels: train labels
-    :param test: test samples
-    :param test_labels: test labels
-    :return: mean accuracy
-    """
-
-    sum = 0
-    for iteration in range(0, NOF_ITERATIONS):
-        n = train.shape[0]
-        nof_features = train.shape[1]
-
-        # Shuffle the data
-        indices_shuffle = np.random.permutation(n)
-        train_shuffle = train[indices_shuffle]
-        train_labels_shuffle = train_labels[indices_shuffle]
-
-        perceptron = Perceptron(nof_features)
-        perceptron.train(train_shuffle, train_labels_shuffle)
-        acc = perceptron.test(test, test_labels)
-        sum += acc
-
-    mean_acc = float(sum) / NOF_ITERATIONS
-    return mean_acc
-
-
-def part_b(org_train, org_train_labels):
-    """
-    This function is calculates part b
-    :param org_train: original train samples
-    :param org_train_labels: original train labels
-    :return: perceptron
-    """
-
-    nof_features = org_train.shape[1]
-    perceptron = Perceptron(nof_features)
-    perceptron.train(org_train, org_train_labels)
-    weights = perceptron.get_weights()
-    plt.figure()
-    plt.imshow(np.reshape(weights, (28, 28)), interpolation='nearest')
-    return perceptron
-
-
-def part_c(perceptron, test, test_labels):
-    """
-    This function calculates part c
-    :param perceptron:
-    :param test:
-    :param test_labels:
-    :return:
-    """
-
-    mean_acc = perceptron.test(test, test_labels)
-    print "Mean accuracy of the full train samples: {}".format(mean_acc)
-
-
-def part_d(perceptron, test, test_labels):
-    """
-    This function calculates part d
-    :param perceptron: perceptron
-    :param test: test samples
-    :param test_labels: test labels
-    :return:
-    """
-
-    # Get the misclassified samples and labels
-    misclassified_samples, misclassified_labels = perceptron.find_misclassified_samples(test, test_labels)
-    # Plot the misclassified samples
-    plt.figure()
-    plt.imshow(np.reshape(misclassified_samples[1], (28, 28)), interpolation='nearest', cmap='gray')
-
-
-if __name__ == '__main__':
-    n_lst = [5, 10, 50, 100, 500, 1000, 5000]
-
-    # Get train, validation and test data
-    org_train, org_train_labels, org_validation, org_validation_labels, org_test, org_test_labels = get_train_validation_test_data()
-
-    part_a(org_train, org_train_labels, org_validation, org_validation_labels)
-
-    # perceptron = part_b(org_train, org_train_labels)
-    # part_c(perceptron, org_test, org_test_labels)
-    # part_d(perceptron, org_test, org_test_labels)
-
-    print 'debug'
+    plt.imshow(np.reshape(svm.coef_, (28, 28)), interpolation='nearest')
